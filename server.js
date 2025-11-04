91eb8bebd2d49c503e161a84e0aa96245f9d9296
El tio del html <Jarano2023@gmail.com>
2025-11-04 10:29:25 +0100

    Use environment variable for server port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const host = 'localhost';
 
 app.use(express.static('wwwroot'));
